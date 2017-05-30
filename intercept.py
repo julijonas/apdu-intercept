ebe3d7960ca9fb754ca0f8646dc53978c8af21c3
Julijonas Kikutis <julijonas.kikutis@gmail.com>
2017-05-30 11:03:22 +0100

    Remove color codes from logging

--- intercept.py
+++ intercept.py
@@ -5,15 +5,25 @@ import datetime
 import socket
 import struct
 import errno
+import re
 
 from relay_os import RelayOS
 from apdu_printer import APDUPrinter
 
 
+
+class ColorCodeRemovingFormatter(logging.Formatter):
+    pattern = re.compile(r'\033\[\d{1,2}m')
+    def format(self, record):
+        s = super(ColorCodeRemovingFormatter, self).format(record)
+        return self.pattern.sub('', s)
+
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler(sys.stdout))
 current_date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-logger.addHandler(logging.FileHandler("logs/{}.log".format(current_date)))
+file_handler = logging.FileHandler("logs/{}.log".format(current_date))
+file_handler.setFormatter(ColorCodeRemovingFormatter())
+logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
 
 
